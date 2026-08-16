41d6e8a744d499f67ec9096b14a414d4d2fe3684
Di4nce <162547551+Di4nce@users.noreply.github.com>
2026-08-16 19:10:40 +0200

    Fix oppgave_4 name, finish oppgave_1 in module 3

--- Module_2/Oppgave_3.py
+++ Module_2/Oppgave_3.py
@@ -1,4 +1,3 @@
 a = "Hallo" # String / Tekst
 b = 10 # Integer / Heltall
 c = 3.1415 # Float / Desimaltall
-d = True # Boolean / Sannhetsverdi
---
+++ Module_3/Oppgave_1.py
@@ -0,0 +1,4 @@
+tall1 = input("Tast tall 1: ")
+tall2 = input("Tast tall 2: ")
+
+print(tall1 + tall2) # Siden vi ikke typekaster, så blir det to tekst etter hverandre eks. 5 + 5 = 55
