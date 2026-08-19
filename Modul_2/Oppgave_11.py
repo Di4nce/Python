eb0bd73acebb5511496da738d48bcb94b2fbb760
Di4nce <162547551+Di4nce@users.noreply.github.com>
2026-08-19 12:02:38 +0200

    Øving med modul 2

---
+++ Modul_2/Oppgave_11.py
@@ -0,0 +1,4 @@
+variabel_1 = 25
+variabel_2 = 14
+
+print(variabel_1 + variabel_2)
---
+++ Modul_2/Oppgave_41.py
@@ -0,0 +1,5 @@
+heltall = 4
+desimaltall = 4.2
+tekstverdi = "42"
+
+summer = heltall + desimaltall + float(tekstverdi)
