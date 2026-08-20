e76ee7e3cfbdec598614ae416616104643a3194c
Di4nce <162547551+Di4nce@users.noreply.github.com>
2026-08-20 12:15:48 +0200

    Start new program rpg.py

---
+++ Egen/rpg.py
@@ -0,0 +1,24 @@
+import random
+
+def d20():
+    return random.randint(1,20)
+def d8():
+    return random.randint(1,8)
+def d6():
+    return random.randint(1,6)
+
+class Player:
+    def __init__(self, name, to_hit, ac):
+        self.name = name
+        self.to_hit = to_hit
+        self.ac = ac
+        # self.hp = d8()
+
+
+print("Welcome to to the RPG-game")
+player_name = input("What is the name of your character? ")
+print(f"Hello, {player_name}!")
+to_hit = int(input("What is your to hit? (enter a number between 0 and 5) "))
+ac = int(input("What is your AC? (enter a number between 10 and 20) "))
+hp = d8()
+print(f"You have created a character named {player_name}, with {ac} AC, +{to_hit} to hit and {hp} HP. Good luck!")
