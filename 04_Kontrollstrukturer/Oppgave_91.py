92a49c6e3b2df4e210b03746a9e78c776dd65f90
Di4nce <162547551+Di4nce@users.noreply.github.com>
2026-08-25 19:03:06 +0200

    Done assigments from book chapter 5 and 9

---
+++ 04_Kontrollstrukturer/Oppgave_91.py
@@ -0,0 +1,9 @@
+tall = 1
+while tall <= 5:
+    print (tall, " * ", tall, " = ", tall*tall)
+    tall += 1
+
+for i in range(1, 6):
+    print (i, " * ", i, " = ", i*i)
+
+# I denne oppgaven var nok for enklest
