8defca1436f5596350013fead871a171589c69f8
Di4nce <162547551+Di4nce@users.noreply.github.com>
2026-09-03 14:14:53 +0200

    Fix error in lecture notes

--- 07_Filer/objekter_metoder.py
+++ 07_Filer/objekter_metoder.py
@@ -2,8 +2,8 @@ strengen = "En test av strenger og metoder"
 en_annen_streng = "En annen streng"
 testestreng = "     en streng med whitespace   \n"
 
-store_bokstaver = strengen.upper
-smaa_bokstaver = strengen.lower
+store_bokstaver = strengen.upper()
+smaa_bokstaver = strengen.lower()
 print(store_bokstaver)
 print(strengen)
 print(en_annen_streng)
@@ -14,10 +14,10 @@ print(strippet)
 hvor = strengen.find("av")
 print(hvor) # printer 8, posisjonen fra bokstaven a (telt fra null), gir -1 om den ikke finner strengen
 print(strengen.isdigit())
-tallstreng = 456
-# print(tallstreng.isdigit())
-tallstreng = 34.23
-# print(tallstreng.isdigit())
+tallstreng = "456"
+print(tallstreng.isdigit())
+tallstreng = "34.23"
+print(tallstreng.isdigit())
 tall_norsk = "345,23"
 tall_engelsk = tall_norsk.replace(",", ".") # Erstatter "," med "."
 print(tall_engelsk)
