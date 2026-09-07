032d8a86c70ce3373d04f80c8588a4735ebddd40
Di4nce <162547551+Di4nce@users.noreply.github.com>
2026-09-07 13:57:31 +0200

    Fix error in oving3

--- Ovinger/Oving_3/d.py
+++ Ovinger/Oving_3/d.py
@@ -1,9 +1,10 @@
 def perfekt(a):
+    sum = 1 # Akkumulator, 1 skal vistnokk altid være med.
     for i in range(2, int(a/2+1)):
         if a % i == 0:  # Litt usikker på matten her, men tolket oppgaven så godt jeg kan og det gir korrekt svar
-            return True
-        else:
-            return False
+            sum += i
+    return sum == a
+        
 
 tall = int(input("Skriv inn et heltall: "))
 perfekt_tall = perfekt(tall)
